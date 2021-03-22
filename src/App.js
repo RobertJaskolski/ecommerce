c5445f10ca94b7ceaa5f6688e35fa2ebf01fb4be
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-03-22 12:10:11 +0100

    Added styles and edit App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import React from "react";
+import "./default.scss";
+import Header from "./components/Header";
+import Homepage from "./pages/Homepage";
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <div className="App">
+      <Header />
+      <div className="main">
+        <Homepage />
+      </div>
+    </div>
+  );
 }
 
 export default App;
