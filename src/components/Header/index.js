ece44a9eb9d20d73d244b4edfe05c451835c8633
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-03-22 18:33:26 +0100

    Edit Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./styles.scss";
 import Logo from "../../assets/logo.png";
 
@@ -7,8 +8,18 @@ function Header() {
     <header className="header">
       <div className="wrap">
         <div className="logo">
-          <img alt="Logo" src={Logo} />
+          <Link to="/">
+            <img alt="Logo" src={Logo} />
+          </Link>
         </div>
+
+        <dvi className="callToActions">
+          <ul>
+            <li>
+              <Link to="/registration">Register</Link>
+            </li>
+          </ul>
+        </dvi>
       </div>
     </header>
   );
