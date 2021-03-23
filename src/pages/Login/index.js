70091779154ea104ed256f7485ca809472801ec7
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-03-23 20:05:12 +0100

    Created login page

---
+++ src/pages/Login/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.scss";
+import SingIn from "../../components/SingIn";
+
+function Login(props) {
+  return (
+    <div>
+      <SingIn />
+    </div>
+  );
+}
+
+export default Login;
