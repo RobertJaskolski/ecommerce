c75073a6584b938dd256066e86eca599c31843f6
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-03-24 16:52:19 +0100

    Implemented Singup
    singup without validation (validation will create in the future)

--- src/components/SingIn/index.js
+++ src/components/SingIn/index.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.scss";
 import Button from "../Forms/Button";
-import { singInWithGoogle } from "../../firebase/utils";
+import { singInWithGoogle, auth } from "../../firebase/utils";
+
+import Input from "../Forms/Input";
 
 function SingIn(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "email") setEmail(value);
+    else if (name === "password") setPassword(value);
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      setEmail("");
+      setPassword("");
+    } catch {
+      console.log(err);
+    }
   };
 
   return (
@@ -14,6 +32,21 @@ function SingIn(props) {
         <h2>LogIn</h2>
         <div className="formWrap">
           <form onSubmit={handleSubmit}>
+            <Input
+              type="email"
+              name="email"
+              value={email}
+              placeholder="E-mail"
+              onChange={handleChange}
+            />
+            <Input
+              type="password"
+              name="password"
+              value={password}
+              placeholder="Password"
+              onChange={handleChange}
+            />
+            <Button type="submit">LogIn</Button>
             <div className="socialSingin">
               <div className="row">
                 <Button onClick={singInWithGoogle}>Sing in with Google</Button>
