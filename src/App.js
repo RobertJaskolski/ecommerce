7fdda140e531b7a1015ef7efb772255c6c570ee3
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-03-26 22:43:57 +0100

    created saga for checking session

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useEffect } from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import "./default.scss";
-import { auth, handleUserProfile } from "./firebase/utils";
-import { setCurrentUser } from "./redux/User/user.actions";
+import { checkUserSession } from "./redux/User/user.actions";
 import { useDispatch } from "react-redux";
 
 // Layouts
@@ -23,23 +22,7 @@ function App(props) {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const authListener = auth.onAuthStateChanged(async (userAuth) => {
-      if (userAuth) {
-        const userRef = await handleUserProfile(userAuth);
-        userRef.onSnapshot((snapshot) => {
-          dispatch(
-            setCurrentUser({
-              id: snapshot.id,
-              ...snapshot.data(),
-            })
-          );
-        });
-      }
-      dispatch(setCurrentUser(userAuth));
-    });
-    return () => {
-      authListener();
-    };
+    dispatch(checkUserSession());
   }, []);
 
   return (
--- src/redux/User/user.sagas.js
+++ src/redux/User/user.sagas.js
@@ -1,7 +1,12 @@
 import { takeLatest, call, all, put } from "redux-saga/effects";
 import userTypes from "./user.types";
 import { emailSignInStart, signInSuccess } from "./user.actions";
-import { auth, handleUserProfile, GoogleProvider } from "../../firebase/utils";
+import {
+  auth,
+  handleUserProfile,
+  GoogleProvider,
+  getCurrentUser,
+} from "../../firebase/utils";
 
 export function* getSnapshotFromUserAuth(user) {
   try {
@@ -27,10 +32,24 @@ export function* emailSignIn({ payload: { email, password } }) {
   }
 }
 
+export function* isUserAuthenticated() {
+  try {
+    const userAuth = yield getCurrentUser();
+    if (!userAuth) return;
+    yield getSnapshotFromUserAuth(userAuth);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export function* onCheckUserSession() {
+  yield takeLatest(userTypes.CHECK_USER_SESSION, isUserAuthenticated);
+}
+
 export function* onEmailSignInStart() {
   yield takeLatest(userTypes.EMAIL_SIGN_IN_START, emailSignIn);
 }
 
 export default function* userSagas() {
-  yield all([call(onEmailSignInStart)]);
+  yield all([call(onEmailSignInStart), call(onCheckUserSession)]);
 }
