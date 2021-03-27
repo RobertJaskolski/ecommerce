2b2c50dbe1d4cdc97641ed43da2967948a65b9ca
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-03-27 17:27:36 +0100

    Added Admin toolbar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import "./default.scss";
 import { checkUserSession } from "./redux/User/user.actions";
 import { useDispatch } from "react-redux";
 
+// Components
+import AdminToolbar from "./components/AdminToolbar";
+
 // Layouts
 import MainLayout from "./layouts/MainLayout";
 import HomepageLayout from "./layouts/HomepageLayout";
@@ -29,6 +32,7 @@ function App(props) {
 
   return (
     <div className="App">
+      <AdminToolbar />
       <Switch>
         <Route
           exact
