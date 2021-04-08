685ad7f8d1d2259f86eec772103d45a92d55afc2
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-04-08 17:59:25 +0200

    Created page for chechout and link that
    - created page for checkout (cart)
    - link that in header and link in app.js

---
+++ src/pages/Cart/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.scss";
+import Checkout from "../../components/Checkout";
+
+function Cart({}) {
+  return (
+    <div>
+      <Checkout />
+    </div>
+  );
+}
+
+export default Cart;
